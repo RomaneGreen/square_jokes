b0dab786d866923981b89d44b58be2a8fd5ab1cc
Romane G <romane71193@gmail.com>
2020-01-29 00:04:19 -0500

    semantically correct code

--- views/home.php
+++ views/home.php
@@ -5,20 +5,10 @@ $data = file_get_contents($url); // put the contents of the file into a variable
 $characters = json_decode($data); // decode the JSON feed
 
 $randNum = rand(0,4);
-echo $characters[$randNum]->name; echo "<br>";
-echo $characters[$randNum]->race;
+echo $characters[$randNum]->Ans; echo "<br>";
+echo $characters[$randNum]->Ques;
 
 ?>
 
-<!-- 
-<table>
-	<tbody>
-	
-		
-        <tr> -->
-            
-        <!-- </tr>
 
-	</tbody>
-</table> -->
 
