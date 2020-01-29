0728accfef0b44e7b7ce3f1b8a33cd9c8bccdcd2
Romane G <romane71193@gmail.com>
2020-01-28 23:13:46 -0500

    lets make our routes more consice

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
+// Grabs the URI and breaks it apart in case we have querystring stuff
+$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
-include_once 'Request.php';
-include_once 'Router.php';
-$router = new Router(new Request);
-
-$router->get('/', function() {
-  return <<<HTML
-  <h1>Hello world</h1>
-HTML;
-});
-
-
-$router->get('/profile', function($request) {
-  return <<<HTML
-  <h1>Profile</h1>
-HTML;
-});
-
-$router->post('/data', function($request) {
-
-  return json_encode($request->getBody());
-});
+// Route it up!
+switch ($request_uri[0]) {
+    // Home page
+    case '/':
+        require './views/home.php';
+        break;
+    // About page
+    case '/about':
+        require './views/about.php';
+        break;
+    // Everything else
+    default:
+        header('HTTP/1.0 404 Not Found');
+        require './views/404.php';
+        break;
+}
