40ccf835ea4e86d928246de205b117a2958845f9
Romane G <romane71193@gmail.com>
2020-01-29 22:50:11 -0500

    just levrage an api for god sake

--- api/jokes.php
+++ api/jokes.php
@@ -2,28 +2,26 @@
 
 $url = 'data.json'; // path to your JSON file
 $data = file_get_contents($url); // put the contents of the file into a variable
-$characters = json_decode($data,true); // decode the JSON feed
-$count = $_GET['count'];
-//echo $count;
-$jokes = 'http://api.icndb.com/jokes/random/'. $count;
-$jokesz= file_get_contents($jokes);
-echo $jokesz;
-//echo $data
+$characters = json_decode($data); // decode the JSON feed
+
+array_slice($data, 2); 
+print_r($data)
+// foreach ($characters as $character) {
+// 	echo $character->name . '<br>';
+// }
 ?>
 
-<!-- <table>
+<table>
 	<tbody>
 		<tr>
 			<th>Question</th>
 			<th>Answer</th>
 		</tr>
-		 <?php foreach ($characters as $character) : ?> 
+		<?php foreach ($characters as $character) : ?>
         <tr>
             <td> <?php echo $character->Ques; ?>  </td>
             <td> <?php echo $character->Ans; ?> </td>
         </tr>
 		<?php endforeach; ?>
-
-    
 	</tbody>
-</table> -->
+</table>
